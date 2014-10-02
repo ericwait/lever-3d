b8325d3c4c005c209b670172c3ea2a898623a172
shihao <ss3695@drexel.edu>
2014-10-02 15:27:12 -0400

    reformatted copyright headers

--- src/matlab/RemoveTrackFromFamily.m
+++ src/matlab/RemoveTrackFromFamily.m
@@ -4,28 +4,28 @@
 % This is not intended to change the structure, just to keep the data
 % correct
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
-%     Copyright 2011 Andrew Cohen, Eric Wait and Mark Winter
+%     Copyright 2014 Andrew Cohen, Eric Wait, and Mark Winter
 %
-%     This file is part of LEVer - the tool for stem cell lineaging. See
-%     https://pantherfile.uwm.edu/cohena/www/LEVer.html for details
+%     This file is part of LEVER 3-D - the tool for 5-D stem cell segmentation,
+%     tracking, and lineaging. See http://bioimage.coe.drexel.edu 'software' section
+% 	  for details.
+%
+%     LEVER 3-D is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by the Free
+%     Software Foundation, either version 3 of the License, or (at your option) any
+%     later version.
 % 
-%     LEVer is free software: you can redistribute it and/or modify
-%     it under the terms of the GNU General Public License as published by
-%     the Free Software Foundation, either version 3 of the License, or
-%     (at your option) any later version.
-% 
-%     LEVer is distributed in the hope that it will be useful,
-%     but WITHOUT ANY WARRANTY; without even the implied warranty of
-%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-%     GNU General Public License for more details.
+%     LEVER 3-D is distributed in the hope that it will be useful, but WITHOUT ANY
+%     WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+%     A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 % 
 %     You should have received a copy of the GNU General Public License
 %     along with LEVer in file "gnu gpl v3.txt".  If not, see 
 %     <http://www.gnu.org/licenses/>.
 %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function RemoveTrackFromFamily(trackID)
 
